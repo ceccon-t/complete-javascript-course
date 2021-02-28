3c1648e4440e46363cf1532bee4724460bc438bb
Tiago Ceccon <ceccon.t@hotmail.com>
2021-02-28 16:25:32 -0300

    Add Coding challenge 1 code

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,4 @@
+console.log("Practice assignment 1: ");
 let country = "Brazil";
 
 let continent = "South America";
@@ -5,3 +6,39 @@ let continent = "South America";
 let population = 209;
 
 console.log("Assignment - Values and Variables: ", country, continent, population);
+
+
+// ==================================================
+console.log("Coding challenge 1: ");
+
+let markWeight, markHeight, markBMI;
+let johnWeight, johnHeight, johnBMI;
+
+let markHigherBMI;
+
+// with test data 1
+markWeight = 78;
+markHeight = 1.69;
+markBMI = markWeight / (markHeight**2);
+
+johnWeight = 92;
+johnHeight = 1.95;
+johnBMI = johnWeight / (johnHeight**2);
+
+markHigherBMI = markBMI > johnBMI;
+
+console.log("Results with test data 1 (mark BMI, john BMI, is mark BMI higher): ", markBMI, johnBMI, markHigherBMI);
+
+// with test data 2
+markWeight = 95;
+markHeight = 1.88;
+markBMI = markWeight / (markHeight**2);
+
+johnWeight = 85;
+johnHeight = 1.76;
+johnBMI = johnWeight / (johnHeight**2);
+
+markHigherBMI = markBMI > johnBMI;
+
+console.log("Results with test data 2 (mark BMI, john BMI, is mark BMI higher): ", markBMI, johnBMI, markHigherBMI);
+
