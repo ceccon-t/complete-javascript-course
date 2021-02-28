c139603c14b084558f58cdf12c18da9ffc95a8b8
Tiago Ceccon <ceccon.t@hotmail.com>
2021-02-28 15:31:37 -0300

    Code to explore data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -7,3 +7,35 @@ console.log(firstName);
 let $function = "This is allowed";
 
 let PI = 3.1415;
+
+// let javascripIsFun = true;
+// console.log(javascripIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascripIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javascripIsFun = 'YES!';
+// console.log(javascripIsFun);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
