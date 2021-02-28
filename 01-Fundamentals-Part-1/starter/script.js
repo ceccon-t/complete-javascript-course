214ba19640d9968eb3fe12d4be6df0e71a2dd459
Tiago Ceccon <ceccon.t@hotmail.com>
2021-02-28 15:57:15 -0300

    Math and comparison operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -40,7 +40,6 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 let age = 30;
 age = 31;
@@ -55,3 +54,39 @@ job = "teacher";
 
 lastName = "Schmedtmann";
 console.log(lastName);
+*/
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2**3);
+// 2**3 means 2 to the power of 3 = 2*2*2
+
+// String operators
+firstName = 'Jonas';
+lastName = 'Schmedtmann';
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+console.log(x);
+x += 10
+console.log(x);
+x *= 4;
+console.log(x);
+x++;
+console.log(x);
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
