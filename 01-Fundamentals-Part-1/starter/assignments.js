d1a84e4d9034ed2e81932990213e770feeae35d2
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-01 21:48:12 -0300

    Add code from Coding Challenge 2

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -42,3 +42,46 @@ markHigherBMI = markBMI > johnBMI;
 
 console.log("Results with test data 2 (mark BMI, john BMI, is mark BMI higher): ", markBMI, johnBMI, markHigherBMI);
 
+
+// ==================================================
+console.log("Coding challenge 2: ");
+
+// with test data 1
+console.log(`With test data 1`);
+markWeight = 78;
+markHeight = 1.69;
+markBMI = markWeight / (markHeight**2);
+
+johnWeight = 92;
+johnHeight = 1.95;
+johnBMI = johnWeight / (johnHeight**2);
+
+markHigherBMI = markBMI > johnBMI;
+
+//console.log("Results with test data 1 (mark BMI, john BMI, is mark BMI higher): ", markBMI, johnBMI, markHigherBMI);
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher that John's (${johnBMI})!`);
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+}
+
+// with test data 2
+console.log(`With test data 2`);
+markWeight = 95;
+markHeight = 1.88;
+markBMI = markWeight / (markHeight**2);
+
+johnWeight = 85;
+johnHeight = 1.76;
+johnBMI = johnWeight / (johnHeight**2);
+
+markHigherBMI = markBMI > johnBMI;
+
+//console.log("Results with test data 2 (mark BMI, john BMI, is mark BMI higher): ", markBMI, johnBMI, markHigherBMI);
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher that John's (${johnBMI})!`);
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+}
+
+
