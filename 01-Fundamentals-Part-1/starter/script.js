8801dd03591e39ccd024c1ef5b0dfca5a5df2b3b
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-03 01:25:17 -0300

    The ternary operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -268,7 +268,6 @@ if (day === 'monday') {
 } else {
     console.log('Not a valid day!');
 }
-*/
 
 3 + 4
 1991
@@ -280,3 +279,21 @@ if (23 > 10) {
 
 const me = 'Jonas';
 console.log(`I'm ${2037 - 1991} years old ${me}`);
+*/
+
+const age = 23;
+age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 ='water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
