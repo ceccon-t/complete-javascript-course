5f978e47f8fd987b443953acc587b2b818981e48
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-03 00:50:42 -0300

    Add code from coding challenge 3

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -85,3 +85,82 @@ if (markHigherBMI) {
 }
 
 
+// ==================================================
+console.log("Coding challenge 3: ");
+
+let dolphins_score_1, dolphins_score_2, dolphins_score_3;
+let koalas_score_1, koalas_score_2, koalas_score_3;
+
+let dolphins_average_score;
+let koalas_average_score;
+
+// with test data 1
+console.log(`With test data 1`);
+dolphins_score_1 = 96;
+dolphins_score_2 = 108;
+dolphins_score_3 = 89;
+koalas_score_1 = 88;
+koalas_score_2 = 91;
+koalas_score_3 = 110;
+
+dolphins_average_score = (dolphins_score_1 + dolphins_score_2+ dolphins_score_3) / 3;
+koalas_average_score = (koalas_score_1 + koalas_score_2 + koalas_score_3) / 3;
+
+console.log(`Dolphin average score: ${dolphins_average_score}, Koalas average score: ${koalas_average_score}`);
+if ((dolphins_average_score > koalas_average_score) && (dolphins_average_score >= 100) ) {
+    console.log("Dophins win.");
+} else if ((koalas_average_score > dolphins_average_score) && (koalas_average_score >= 100)) {
+    console.log("Koalas win.");
+} else if ( (dolphins_average_score >= 100) && (koalas_average_score >= 100) ) {
+    console.log("It was a draw.");
+} else {
+    console.log("No team wins.");
+}
+
+// with test data 2
+console.log(`With test data 2`);
+dolphins_score_1 = 97;
+dolphins_score_2 = 112;
+dolphins_score_3 = 101;
+koalas_score_1 = 109;
+koalas_score_2 = 95;
+koalas_score_3 = 123;
+
+dolphins_average_score = (dolphins_score_1 + dolphins_score_2+ dolphins_score_3) / 3;
+koalas_average_score = (koalas_score_1 + koalas_score_2 + koalas_score_3) / 3;
+
+console.log(`Dolphin average score: ${dolphins_average_score}, Koalas average score: ${koalas_average_score}`);
+if ((dolphins_average_score > koalas_average_score) && (dolphins_average_score >= 100) ) {
+    console.log("Dophins win.");
+} else if ((koalas_average_score > dolphins_average_score) && (koalas_average_score >= 100)) {
+    console.log("Koalas win.");
+} else if ( (dolphins_average_score >= 100) && (koalas_average_score >= 100) ) {
+    console.log("It was a draw.");
+} else {
+    console.log("No team wins.");
+}
+
+// with test data 3
+console.log(`With test data 3`);
+dolphins_score_1 = 97;
+dolphins_score_2 = 112;
+dolphins_score_3 = 101;
+koalas_score_1 = 109;
+koalas_score_2 = 95;
+koalas_score_3 = 106;
+
+dolphins_average_score = (dolphins_score_1 + dolphins_score_2+ dolphins_score_3) / 3;
+koalas_average_score = (koalas_score_1 + koalas_score_2 + koalas_score_3) / 3;
+
+console.log(`Dolphin average score: ${dolphins_average_score}, Koalas average score: ${koalas_average_score}`);
+if ((dolphins_average_score > koalas_average_score) && (dolphins_average_score >= 100) ) {
+    console.log("Dophins win.");
+} else if ((koalas_average_score > dolphins_average_score) && (koalas_average_score >= 100)) {
+    console.log("Koalas win.");
+} else if ( (dolphins_average_score >= 100) && (koalas_average_score >= 100) ) {
+    console.log("It was a draw.");
+} else {
+    console.log("No team wins.");
+}
+
+
