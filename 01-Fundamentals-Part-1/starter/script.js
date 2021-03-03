8b76c8c7bf60223e0514d2af0a5fcfefdeb5ec1c
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-02 21:38:40 -0300

    Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -124,7 +124,6 @@ lines');
 console.log(`String with
 multiple
 lines`);
-*/
 
 const age = 15;
 
@@ -143,4 +142,24 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
+*/
 
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23));
+
+// type coercion
+console.log("I am " + 23 + " years old.");
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+console.log('23' > '18');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
