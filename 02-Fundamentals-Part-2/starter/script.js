a9e8f4d19f60e44475e8086eeb8221b1f36b9a71
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-04 00:55:38 -0300

    Strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; // throws error (undeclared variable)
+if (hasDriversLicense) console.log('I can drive :D');
+
+const interface = 'Audio'; // throws error (reserved word for future)
+const private = 534; // throws error (reserved word for future)
