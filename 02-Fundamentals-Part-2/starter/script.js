545a5daa51ceb3b2da2d7882cb0a5f466a7c67a6
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-04 01:19:27 -0300

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,7 +9,6 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 const interface = 'Audio'; // throws error (reserved word for future)
 const private = 534; // throws error (reserved word for future)
-*/
 
 function logger() {
     console.log('My name is Jonas');
@@ -30,3 +29,18 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+*/
+
+// Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
