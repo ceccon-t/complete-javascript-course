9eabb7c83f076c40a265c920a6e8841931aebe6c
Tiago Ceccon <ceccon.t@hotmail.com>
2021-04-09 00:50:01 -0300

    Nested loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -251,6 +251,7 @@ for (let rep = 1; rep <= 10; rep++) {
 }
 */
 
+/*
 const jonas = [
     'Jonas',
     'Schmedtmann',
@@ -291,4 +292,25 @@ console.log('--- BREAK WITH NUMBER ---');
 for (let i = 0; i < jonas.length; i++) {
     if (typeof jonas[i] === 'number') break;
     console.log(jonas[i], typeof jonas[i]);
+}
+*/
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037-1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+for (let i = jonas.length-1; i >= 0; i--) {
+    console.log(jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`------ Starting exercise ${exercise}`);
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+    }
 }
