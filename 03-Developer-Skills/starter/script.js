f29133eb2fae5ba15613da7eef2a4e2dfccb263e
Tiago Ceccon <ceccon.t@hotmail.com>
2021-05-31 17:34:58 -0300

    Add debugging code

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,9 +1,33 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
+/*
+const x = '23';
 if (x === 23) console.log(23);
 
 const calcAge = birthYear => 2037 - birthYear;
 
 // VSCode user snippets folder on Linux: // home/<user>/.config/Code/User/snippets
+
+console.log(calcAge(1991));
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // C) Fix
+    value: Number(prompt('Degree celsius: ')),
+  };
+
+  // B) Find
+  console.log(measurement);
+  console.table(measurement);
+
+  const kelvin = measurement.value + 273;
+
+  return kelvin;
+};
+
+// A) Identify bug
+console.log(measureKelvin());
