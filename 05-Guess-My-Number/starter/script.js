12060035c9abca76c3aeec0ab7b50db09ffb110c
Tiago Ceccon <ceccon.t@hotmail.com>
2021-06-08 22:21:20 -0300

    Add solution to coding challenge 1 of section 5

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,11 +14,40 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+/* Video code
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
+console.log(`Secret number is ${secretNumber}`);
 
 let score = 20;
 document.querySelector('.score').textContent = score;
+*/
+
+/* Implementation of coding challenge #1 */
+let secretNumber;
+let score;
+
+const setupGame = () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  let numberElement = document.querySelector('.number');
+  numberElement.textContent = '?';
+  numberElement.style.width = '15rem';
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('.score').textContent = score;
+};
+
+setupGame();
+
+document.querySelector('.again').addEventListener('click', setupGame);
+/* End implementation of coding challenge #1 */
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -32,6 +61,7 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
 
     // When guess is too high
