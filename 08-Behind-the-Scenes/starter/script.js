37fe36d3b5b60463ce24a8403d1323ae9742f396
Tiago Ceccon <ceccon.t@hotmail.com>
2021-06-18 13:58:10 -0300

    Reference types behavior

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -121,6 +121,7 @@ const f = jonas.calcAge;
 // f();
 */
 
+/*
 var firstName = 'Matilda';
 
 const jonas = {
@@ -163,4 +164,20 @@ var addArrow = (a, b) => {
     return a + b;
 };
 addArrow(2, 5, 8);
+*/
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+    name: 'Jonas',
+    age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend: ', friend);
+console.log('Me: ', me);
 
