d7aa3df718835bbd5ac5d4dd4b829542039f4345
Tiago Ceccon <ceccon.t@hotmail.com>
2021-06-22 19:16:19 -0300

    Short circuiting

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -44,6 +44,37 @@ const restaurant = {
   },
 };
 
+console.log('------- OR -------');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(null || undefined);
+console.log( (null || undefined) === (undefined || null) ); // seriously, Javascript, why? WHY????
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('------- AND -------');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'Jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushroom', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
+
+/*
 // SPREAD, because on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
 
@@ -76,6 +107,7 @@ add(...x);
 
 restaurant.orderPizza('mushroom', 'onion', 'olives', 'spinach');
 restaurant.orderPizza('mushroom');
+*/
 
 /*
 //////////////////////////
