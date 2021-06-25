457a1c89b8dd03120a479c9050dca746ec1635aa
Tiago Ceccon <ceccon.t@hotmail.com>
2021-06-24 23:26:58 -0300

    Looping on keys, values and entries

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -50,6 +50,31 @@ const restaurant = {
   },
 };
 
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, {open, close}] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
+
+/*
 // With optional chaining
 console.log(restaurant.openingHours?.mon?.open);
 
@@ -70,6 +95,7 @@ console.log(restaurant.orderRisotto?.(0,1) ?? 'Method does not exist' );
 const users = [{name: 'Jonas', email: 'hello@jonas.io'}];
 
 console.log(users[0]?.name ?? 'User array empty');
+*/
 
 /*
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
