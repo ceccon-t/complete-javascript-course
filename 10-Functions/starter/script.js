0f5cb2f75a9d51669b3d45213d6c9114e3800380
Tiago Ceccon <ceccon.t@hotmail.com>
2021-06-30 16:35:39 -0300

    Pass by value vs pass by reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 
 const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers) {
@@ -21,4 +22,38 @@ createBooking('LH123');
 createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
+*/
 
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 24739479284
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if (passenger.passport === 24739479284) {
+        alert('Check in');
+    } else {
+        alert('Wrong passport!');
+    }
+}
+
+checkIn(flight, jonas);
+
+console.log(flight);
+console.log(jonas);
+
+// Is the same as doing...
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function(person) {
+    person.passport = Math.trunc( Math.random() * 10000000 );
+}
+
+newPassport(jonas);
+
+checkIn(flight, jonas);
