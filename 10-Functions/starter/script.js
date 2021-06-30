f777d1e676798d22dea3581a1f9814fc403136a7
Tiago Ceccon <ceccon.t@hotmail.com>
2021-06-30 17:10:32 -0300

    The bind method

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -157,3 +157,45 @@ console.log(swiss);
 
 book.call(swiss, ...flightData);
 console.log(swiss);
+
+// Bind method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
+console.log(eurowings);
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function() {
+    console.log(this);
+    this.planes++;
+    console.log(this.planes);
+}
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function(rate) {
+    return function(value) {
+        return value + value * rate;
+    }
+}
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
