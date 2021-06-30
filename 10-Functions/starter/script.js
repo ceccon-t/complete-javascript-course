10f6491cd799604ff3c9494a656801fd2ffbb728
Tiago Ceccon <ceccon.t@hotmail.com>
2021-06-30 16:45:57 -0300

    Functions accepting callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -24,6 +24,7 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 */
 
+/*
 const flight = 'LH234';
 const jonas = {
     name: 'Jonas Schmedtmann',
@@ -43,12 +44,12 @@ const checkIn = function(flightNum, passenger) {
 
 checkIn(flight, jonas);
 
-console.log(flight);
-console.log(jonas);
+// console.log(flight);
+// console.log(jonas);
 
 // Is the same as doing...
-const flightNum = flight;
-const passenger = jonas;
+// const flightNum = flight;
+// const passenger = jonas;
 
 const newPassport = function(person) {
     person.passport = Math.trunc( Math.random() * 10000000 );
@@ -57,3 +58,33 @@ const newPassport = function(person) {
 newPassport(jonas);
 
 checkIn(flight, jonas);
+*/
+
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+// Higher-order function
+const transformer = function(str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function() {
+    console.log('HI');
+}
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+
