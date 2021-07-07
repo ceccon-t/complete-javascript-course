a8cf81942505f247c37223215b3a407a86211927
Tiago Ceccon <ceccon.t@hotmail.com>
2021-07-07 19:53:18 -0300

    The map method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -170,3 +170,26 @@ currenciesUnique.forEach(function(value, _, map) {
   console.log(`${value}: ${value}`); // 'key' does not make sense for sets
 });
 */
+
+// THE MAP METHOD
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) {
+  movementsUSDfor.push(mov * eurToUsd);
+}
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov, i) => {
+  return `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`;
+});
+console.log(movementsDescriptions);
+
+
