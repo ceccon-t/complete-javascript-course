64f321cae5cf8f71ed1622860b63de0cbd9372f0
Tiago Ceccon <ceccon.t@hotmail.com>
2021-07-08 14:42:08 -0300

    Implement login

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,33 +75,29 @@ const displayMovements = function(movements) {
   });
 }
 
-displayMovements(account1.movements);
-
 const calcDisplayBalance = function(movements) {
   const balance = movements.reduce((acc, mov) => acc+mov, 0);
   labelBalance.textContent = `${balance} EUR`;
 }
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function(movements) {
-  const incomes = movements
+const calcDisplaySummary = function(acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0 );
   labelSumIn.textContent = `${incomes} EUR`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)} EUR`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => deposit * 1.2/100)
+    .map(deposit => (deposit * acc.interestRate) /100)
     .filter((int) => int >= 1)
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest} EUR`;
 }
-calcDisplaySummary(account1.movements);
 
 const createUsernames = function(accounts) {
   accounts.forEach(function(acc) {
@@ -109,7 +105,35 @@ const createUsernames = function(accounts) {
   });
 }
 
-createUsernames(accounts)
+createUsernames(accounts);
+
+// Event handler
+let currentAccount;
+
+btnLogin.addEventListener('click', function(e) {
+  // Prevent form from submitting
+  e.preventDefault();
+  currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`;
+    containerApp.style.opacity = 100;
+
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // Display movements
+    displayMovements(currentAccount.movements);
+
+    // Display balance
+    calcDisplayBalance(currentAccount.movements);
+
+    // Display summary
+    calcDisplaySummary(currentAccount);
+  }
+});
 
 
 /////////////////////////////////////////////////
@@ -296,7 +320,7 @@ const totalDepositsUSD = movements
 console.log(totalDepositsUSD);
 */
 
-
+/*
 // THE FIND METHOD
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -309,3 +333,4 @@ console.log(accounts);
 
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account);
+*/
