f58d49eb2e0cde3f71b5407046307d48e3a0fe3f
Tiago Ceccon <ceccon.t@hotmail.com>
2021-07-08 18:18:47 -0300

    More ways of creating and filling arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -446,7 +446,7 @@ const overallBalance2 = accounts
 console.log(overallBalance2);
 */
 
-
+/*
 // Sorting arrays
 // Strings
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
@@ -482,3 +482,40 @@ console.log(movements);
 // Descending with gambiarra
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+
+
+// More ways of creating and filling arrays
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const q = new Array(7);
+console.log(q);
+console.log(q.map(() => 5));
+
+q.fill(1);
+console.log(q);
+
+const x = new Array(7);
+console.log(x);
+
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({length: 7}, () => 1);
+console.log(y);
+
+const z = Array.from({length: 7}, (_, i) => i+1);
+console.log(z);
+
+labelBalance.addEventListener('click', function() {
+  const movementsUI = Array.from(document.querySelectorAll('.movements__value'), el => Number(el.textContent.replace('0', '9')));
+
+  console.log(movementsUI);
+});
+
