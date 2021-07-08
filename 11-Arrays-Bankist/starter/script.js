bfbdac2dea9b0c3421c2349ab7062afefbdb8924
Tiago Ceccon <ceccon.t@hotmail.com>
2021-07-08 14:55:49 -0300

    Bankist - Implement transfers

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,9 +75,9 @@ const displayMovements = function(movements) {
   });
 }
 
-const calcDisplayBalance = function(movements) {
-  const balance = movements.reduce((acc, mov) => acc+mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+const calcDisplayBalance = function(acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc+mov, 0);
+  labelBalance.textContent = `${acc.balance} EUR`;
 }
 
 const calcDisplaySummary = function(acc) {
@@ -107,6 +107,17 @@ const createUsernames = function(accounts) {
 
 createUsernames(accounts);
 
+const updateUI = function(acc) {
+    // Display movements
+    displayMovements(acc.movements);
+
+    // Display balance
+    calcDisplayBalance(acc);
+
+    // Display summary
+    calcDisplaySummary(acc);
+}
+
 // Event handler
 let currentAccount;
 
@@ -124,14 +135,25 @@ btnLogin.addEventListener('click', function(e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
-    // Display movements
-    displayMovements(currentAccount.movements);
+    updateUI(currentAccount);
+  }
+});
 
-    // Display balance
-    calcDisplayBalance(currentAccount.movements);
+btnTransfer.addEventListener('click', function(e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value); 
+  inputTransferAmount.value = inputTransferTo.value = '';
 
-    // Display summary
-    calcDisplaySummary(currentAccount);
+  if (amount > 0 && 
+        receiverAcc &&
+        currentAccount.balance >= amount && 
+        receiverAcc?.username !== currentAccount.username) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    
+    updateUI(currentAccount);
   }
 });
 
