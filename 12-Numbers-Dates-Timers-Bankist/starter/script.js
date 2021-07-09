78edc09cfbafab172cd361678c7b29eca0b0b22e
Tiago Ceccon <ceccon.t@hotmail.com>
2021-07-09 00:29:00 -0300

    Creating dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -360,7 +360,7 @@ labelBalance.addEventListener('click', () => {
 });
 */
 
-
+/*
 // WORKING WITH BIGINT
 console.log(2 ** 53 - 1);
 console.log(Number.MAX_SAFE_INTEGER);
@@ -397,6 +397,44 @@ console.log(huge + ' is REALLY big!!');
 // Division with BigInt cuts off decimal part
 console.log(10n / 3n);
 console.log(10 / 3);
+*/
+
+
+// CREATING DATES
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('Aug 02 2020 18:05:41'));
+// console.log(new Date('December 24, 2015'));
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+// console.log(new Date(2037, 10, 31));
+
+// // Miliseconds since the beginning of UNIX time
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23, 5);
+console.log(future);
+console.log(future.getYear());
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142267785000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
 
 
 
